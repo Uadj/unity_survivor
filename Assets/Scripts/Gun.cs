3bb73e44527d0045df2aedf970572a5a9ba449cf
dgw <30551889+Uadj@users.noreply.github.com>
2021-11-19 07:04:30 +0900

    2
    2

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public string gunName; //총이름
+    public float range; //사정거리
+    public float accuracy; //정확도
+    public float fireRate; //연사속도
+    public float reloadTime; //재장전 ㅗㄱ도
+    public int damage;
+    public int reloadBulletCount; //총알 재장전 갯수 탄알집 크기
+    public int currentBulletCount; // 탄알집에 남아있는 갯수
+    public int maxBulletCount; //최대 소지가능 탄알
+    public int carryBulletCount; // 현재 소유 탄알
+    public float retroActionForce; // 반동세기
+    public float retroActionFineSightForce; //정조준 반반동세기
+    public Vector3 fineSightOriginPos; // 정조준 전 원래 위치
+    public Animator anim; //총 애니메이션
+    public ParticleSystem muzzleFlash; //총구화염
+    public AudioClip fire_Sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    [SerializeField]
+    private Gun currentGun;
+
+    private float currentFireRate;
+
+    void Update()
+    {
+        GunFireRateCalc();
+    }
+
+    private void GunFireRateCalc()
+    {
+        if (currentFireRate > 0)
+        {
+            currentFireRate -= Time.deltaTime;
+            TryFire();
+        }
+    }
+    private void TryFire()
+    {
+        if (Input.GetButton("Fire1") && currentFireRate <= 0)
+        {
+            Fire();
+        }
+    }
+    private void Fire()
+    {
+        currentFireRate = currentGun.fireRate;
+        Shoot();
+    }
+    private void Shoot()
+    {
+        Debug.Log("발사");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+ 
+}
